193611a3d258d8ecfb4d41b7e9272cec45a16503
Richard Clarke <freshnewrich@gmail.com>
2022-06-04 11:32:02 -0700

    list.jsx finished

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,19 +2,22 @@ export default function List(props) {
   //takes formstate, passes it down 
   //takes items array to create items
   // {
-      //     title: "",
-      //         url: ""
-      // }
-      //map over the array
-      
-      
-      //props.links.map
-      <Item 
-      formState={props.formState} 
-      setFormState={props.setFormState} 
-      url={props.url} 
-      title={props.title}
-      delete={props.delete}
+  //     title: "",
+  //         url: ""
+  // }
+  //map over the array
+  return <div className="list">
+    {props.links.map((entry) => (
+      <Item
+        formState={props.formState}
+        setFormState={props.setFormState}
+        delete={props.delete}
+        url={entry.url}
+        title={entry.title}
       />
+    ))}
+  </div>
+
+
 
 }
