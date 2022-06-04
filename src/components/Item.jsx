030f0997b346786e35e45f3a7de02ac451cf5170
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-06-04 14:40:26 -0700

    adds state to form and edit function

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,14 @@
 
 export default function Item(props) {
-    
+
+
+
+
 return (
    <div className="item">
        <a href={props.url}>{props.title}</a>
-       <div onClick={() =>{ props.editLink(props.id, props.url, props.title )}}>E</div>
-      <div onClick={() => { props.deleteLink(props.id)}}>X</div>
+       <button className="btn" onClick={() =>{ props.editLink(props.id, props.url, props.title )}}>E</button>
+      <button className="btn" onClick={() => { props.deleteLink(props.id)}}>X</button>
    </div>
 
 )
