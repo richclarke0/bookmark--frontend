321b1c1865f632d6bf22eb07c6002890ed66289e
Richard Clarke <freshnewrich@gmail.com>
2022-06-04 10:55:07 -0700

    list and item

---
+++ src/List.jsx
@@ -0,0 +1,12 @@
+export default function List(props) {
+    //takes formstate, passes it down 
+    //takes items array to create items
+    // {
+        //     title: "",
+        //         url: ""
+        // }
+        
+        //map over the array
+        <Item formState={props.formState}/>
+    
+}
---
+++ src/components/Item.jsx
@@ -0,0 +1,7 @@
+export default function Item(props) {
+    //uses formstate
+    //takes individual items from the array (used in map)
+    // this is how item is called in list
+    //<Item formState={props.formState} url={url} title={title}/>
+
+}
