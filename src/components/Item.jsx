c005fe8901ad78cad321754c8c0fd6662474aecc
Richard Clarke <freshnewrich@gmail.com>
2022-06-04 10:57:06 -0700

    comment item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,6 +2,10 @@ export default function Item(props) {
     //uses formstate
     //takes individual items from the array (used in map)
     // this is how item is called in list
-    //<Item formState={props.formState} url={url} title={title}/>
-
+    //<Item formState={props.formState} url={props.url} title={props.title}/>
+ //your props variables are 
+//  props.formState
+//  props.setFormState
+//  props.url 
+//  props.title
 }
