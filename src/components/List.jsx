251e93e4d806a85491dc6c7cf15f5975bcd31c93
Richard Clarke <freshnewrich@gmail.com>
2022-06-04 11:19:44 -0700

    move list to components

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,20 @@
-
-
-const List = () => {
-  return (
-    <div>
-
+export default function List(props) {
+  //takes formstate, passes it down 
+  //takes items array to create items
+  // {
+      //     title: "",
+      //         url: ""
+      // }
+      //map over the array
       
-    </div>
-  )
+      
+      //props.links.map
+      <Item 
+      formState={props.formState} 
+      setFormState={props.setFormState} 
+      url={props.url} 
+      title={props.title}
+      delete={props.delete}
+      />
+
 }
-export default List
