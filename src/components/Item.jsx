e7ede5605629e001dcdab7d36e6cb6e7f0b011aa
Richard Clarke <freshnewrich@gmail.com>
2022-06-04 13:22:20 -0700

    wendy commit cleanup item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,8 +4,8 @@ export default function Item(props) {
 return (
    <div className="item">
        <a href={props.url}>{props.title}</a>
-       <div onClick={() =>{editLink(props.id, props.url, props.title )}}>E</div>
-      <div onClick={() => { deleteLink(props.id)}}>X</div>
+       <div onClick={() =>{ props.editLink(props.id, props.url, props.title )}}>E</div>
+      <div onClick={() => { props.deleteLink(props.id)}}>X</div>
    </div>
 
 )
